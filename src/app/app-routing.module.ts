a81ddb19eefe201f0eda27a3b0a42cc0e0ecf1a9
Janosch Adams <janosch.adams1993@gmail.com>
2023-06-22 11:12:49 +0200

    test url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import {ImprintComponent} from "./imprint/imprint.component";
 import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {GpcComponent} from "./gpc/gpc.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   { path: 'imprint', component: ImprintComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'sign-in', component: SignInComponent },
+  { path: '/sign-in', component: SignInComponent },
   { path: 'gpc', component: GpcComponent },
-  { path: 'oauth2/redirect', component: GpcComponent }
+  { path: 'oauth2/redirect', component: GpcComponent },
+  { path: '/oauth2/redirect', component: GpcComponent },
+  { path: '**', component: AppComponent }
 ];
 
 @NgModule({
