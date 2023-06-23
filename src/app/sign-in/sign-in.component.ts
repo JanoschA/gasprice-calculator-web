9b770d72cf0344435fd03a74d32b9af7fa7a368d
Janosch Adams <janosch.adams1993@gmail.com>
2023-06-23 11:07:04 +0200

    test angular environment with fileReplacement

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {environment} from "../../environment";
+import {environment} from "../../environment/environment";
 
 @Component({
   selector: 'app-sign-in',
