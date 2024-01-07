ac4476248a1164eef3917e65f6c3a169bb426bc9
Janosch Adams <janosch.adams1993@gmail.com>
2024-01-07 08:33:56 +0100

    feat: Update HeaderComponent to subscribe to AuthService.user$
    - Injected AuthService into HeaderComponent
    - Subscribed to AuthService.user$ in ngOnInit method
    - Updated user data in template to use local user property
    - HeaderComponent now updates user data reactively after login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from '../gpc/User';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  public user$: Observable<User | null> = of(null);
+export class HeaderComponent implements OnInit, OnDestroy {
+  public user: User | null = null;
+  private subscription: Subscription = new Subscription();
 
-  constructor() {}
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
-    this.user$ = this.getUser();
+    this.subscription = this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
   }
 
-  public getUser(): Observable<User | null> {
-    let user = localStorage.getItem("user");
-    if (user != null) {
-      return of(JSON.parse(user));
-    } else {
-      return of(null);
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 }
