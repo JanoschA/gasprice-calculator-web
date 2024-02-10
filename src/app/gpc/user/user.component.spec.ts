ad2d40aaae77aa781fca932a85d01e778c661d7c
Janosch Adams <janosch.adams1993@gmail.com>
2024-02-10 16:35:31 +0100

    changed the user.component.spec.ts

--- src/app/gpc/user/user.component.spec.ts
+++ src/app/gpc/user/user.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
@@ -10,6 +9,10 @@ describe('UserComponent', () => {
     TestBed.configureTestingModule({
       declarations: [UserComponent]
     });
+
+    // Mock localStorage.getItem
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify({ email: 'test@example.com' }));
+
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +20,9 @@ describe('UserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have user email as test@example.com', () => {
+    expect(component.user.email).toEqual('test@example.com');
   });
 });
